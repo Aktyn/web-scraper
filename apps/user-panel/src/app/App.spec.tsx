5003d769440e2d9fa2d07b45c208b70970afccde
Aktyn <Aktyn3@gmail.com>
2023-02-12 00:39:49 +0100

    Mock electronAPI

--- apps/user-panel/src/app/App.spec.tsx
+++ apps/user-panel/src/app/App.spec.tsx
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react'
+import { vi } from 'vitest'
 import App from './App'
 
 describe(App.name, () => {
+  beforeAll(() => {
+    Object.assign(window, {
+      electronAPI: {
+        dummyEvent: () => Promise.resolve(1),
+        dummyEventFromMain: vi.fn(),
+      },
+    })
+  })
+
   it('renders learn react link', () => {
     render(<App />)
     const linkElement = screen.getByText(/Electron communication test/i)
