f642e12e45df40ce0232e951afeb0cb9db417fb0
Aktyn <Aktyn3@gmail.com>
2023-03-28 00:04:53 +0200

    Update packages

--- apps/desktop-app/src/utils/common.ts
+++ apps/desktop-app/src/utils/common.ts
@@ -2,7 +2,6 @@ import path from 'path'
 
 export const EXTERNAL_DIRECTORY_PATH = path.join(__dirname, '..', '..', 'external')
 
-//TODO: support for other platforms
 export const EXECUTABLE_DIRECTORY_PATH = path.normalize(
   process.env.PORTABLE_EXECUTABLE_DIR ||
     (process.env.APPIMAGE ? path.dirname(process.env.APPIMAGE) : path.resolve('.')),
