ecb289e82e3cc2952b2e89062d26a4b25f37a240
Aktyn <Aktyn3@gmail.com>
2023-05-24 18:12:07 +0200

    Update packages

--- apps/user-panel/src/app/themes/helpers.spec.ts
+++ apps/user-panel/src/app/themes/helpers.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { generateComplementaryColor, mixColors, setSaturation } from './helpers'
+import { generateComplementaryColor, mixColors, rotateHue, setSaturation } from './helpers'
 
 describe('mixColors', () => {
   it('should return mixed color by given factor', () => {
@@ -19,6 +19,25 @@ describe('setSaturation', () => {
   it('should return color with given saturation', () => {
     expect(setSaturation('#6a1b9a', 0.35)).toBe('rgb(48,29,59)')
     expect(setSaturation('#00838f', 0.15)).toBe('rgb(9,11,12)')
+  })
+})
+
+describe('rotateHue', () => {
+  it('should rotate hue by 180 degrees', () => {
+    expect(rotateHue('#ff0000', 180)).toBe('#00ffff')
+    expect(rotateHue('#00ff00', 180)).toBe('#ff00ff')
+    expect(rotateHue('#0000ff', 180)).toBe('#ffff00')
+  })
+
+  it('should handle alpha channel', () => {
+    expect(rotateHue('#ff0000ff', 180)).toBe('#00ffffff')
+    expect(rotateHue('#00ff00ff', 180)).toBe('#ff00ffff')
+    expect(rotateHue('#0000ffff', 180)).toBe('#ffff00ff')
+  })
+
+  it('should handle degrees outside of 0-360 range', () => {
+    expect(rotateHue('#ff0000', 540)).toBe('#00ffff')
+    expect(rotateHue('#ff0000', -180)).toBe('#00ffff')
   })
 })
 
