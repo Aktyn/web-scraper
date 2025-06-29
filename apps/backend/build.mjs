09ce78c61601f2a6d49721390c12fdecae57a27e
Aktyn <aktyn3@gmail.com>
2025-06-29 23:05:22 +0200

    Standalone build fix

--- apps/backend/build.mjs
+++ apps/backend/build.mjs
@@ -1,23 +1,25 @@
+// @ts-check
+
 import * as esbuild from "esbuild";
-import { readFile, cp } from "fs/promises";
 import { createRequire } from "module";
+import fs from "node:fs";
 
 const require = createRequire(import.meta.url);
 
 // This plugin is to exclude .node files from the bundle
 const nativeNodeModulesPlugin = {
-    name: 'native-node-modules',
-    setup(build) {
-      build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => {
-        return ({
-            path: require.resolve(args.path, { paths: [args.resolveDir] }),
-            external: true,
-        })
-      });
-    },
+  name: 'native-node-modules',
+  setup(build) {
+    build.onResolve({ filter: /\.node$/, namespace: 'file' }, (args) => {
+      return ({
+        path: require.resolve(args.path, { paths: [args.resolveDir] }),
+        external: true,
+      })
+    });
+  },
 };
 
-const pkg = JSON.parse(await readFile("package.json", "utf-8"));
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 
 const entryPoints = ["src/main.ts"];
 
@@ -25,18 +27,30 @@ const external = [
   ...Object.keys(pkg.devDependencies || {}),
 ];
 
+const outfile = "dist/standalone.js";
+
 await esbuild.build({
   entryPoints,
   bundle: true,
   platform: "node",
   target: "node23",
   format: "cjs",
-  outfile: "dist/standalone.js",
+  outfile,
   external,
   logLevel: "info",
   plugins: [nativeNodeModulesPlugin],
 });
 
-await cp("../../node_modules/@libsql", "./dist/node_modules/@libsql", {
+fs.writeFileSync(
+  outfile,
+  fs
+    .readFileSync(outfile, "utf-8")
+    .replace(
+      /var __filename\d+ = import_node_url\.default\.fileURLToPath\(import_meta\d+\.url\);\s*var __dirname\d+ = import_node_path\d+\.default\.dirname\(__filename\d+\);\s*var (require\d+) = import_node_module\d+\.default\.createRequire\(import_meta\d+\.url\);/g,
+      'var $1 = () => ({version: "2.0.0"})',
+    ).replace(/return \(\) =\> __dirname\d;/g, "return () => __dirname;"),
+);
+
+fs.cpSync("../../node_modules/@libsql", "./dist/node_modules/@libsql", {
   recursive: true,
 }); 
--- build-sea.sh
+++ build-sea.sh
@@ -7,16 +7,23 @@ cp -r ./apps/backend/dist/standalone.js ./apps/backend/dist/standalone-copy.js
 echo "... done copying bundled backend code"
 
 echo "Adjusting backend code before SEA build..."
-sed -i 's/return require(`@libsql\/${target}`);/const sea = require("node:sea")\
+sed -i '1i\
+const sea = require("node:sea")\
 const path = require("node:path")\
 const fs = require("node:fs")\
 if (sea.isSea()) {\
   const libsqlNode = sea.getAsset("libsql.node")\
   const libsqlNodePath = path.join(__dirname, "libsql.node")\
   fs.writeFileSync(libsqlNodePath, Buffer.from(libsqlNode))\
-}\
 \
-return require("node:module").createRequire(__filename)(__dirname + "\/libsql");/g' ./apps/backend/dist/standalone-copy.js
+  const sharpNode = sea.getAsset("sharp.node")\
+  fs.mkdirSync(path.join(__dirname, "lib"), { recursive: true })\
+  const sharpNodePath = path.join(__dirname, "lib", "sharp.node")\
+  fs.writeFileSync(sharpNodePath, Buffer.from(sharpNode))\
+}\
+' ./apps/backend/dist/standalone-copy.js
+sed -i 's/return require(`@libsql\/${target}`);/return require("node:module").createRequire(__filename)(__dirname + "\/libsql");/g' ./apps/backend/dist/standalone-copy.js
+sed -i 's/sharp = require(path[0-9]*);/sharp = require("node:module").createRequire(__filename)(__dirname + "\/lib\/sharp");/g' ./apps/backend/dist/standalone-copy.js
 echo "... done adjusting backend code before SEA build"
 
 echo "Building SEA bundle..."
@@ -24,7 +31,8 @@ node --experimental-sea-config sea-config.json
 echo "... done building SEA bundle"
 
 echo "Removing copy of bundled backend code..."
-rm -rf ./apps/backend/dist/standalone-copy.js
+# TODO: restore
+# rm -rf ./apps/backend/dist/standalone-copy.js
 echo "... done removing copy of bundled backend code"
 
 echo ""
